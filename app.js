1346c5b726758da77c02748025d2b491c1e740f1
marusoft <alimi.kmaruf@gmail.com>
2019-11-05 09:59:33 +0100

    resolve eslint error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: false,
 }));
 
 
