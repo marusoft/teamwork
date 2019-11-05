cbc85b2bf2c458c8e217cf2c96fc79adc92d7edf
marusoft <alimi.kmaruf@gmail.com>
2019-11-05 09:48:21 +0100

    ch(create folder structure): create folder structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 
 const app = express();
@@ -10,12 +10,12 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: false,
 }));
 
 
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to teamwork-app, a place where beautiful things can be achieved',
+  message: 'Welcome to teamwork-app, a place where beautiful things can be achieved through collaboration',
 }));
 
 
