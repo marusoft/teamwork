79b63691aaa0146dace2bd9c4d911afd4ac0d6da
marusoft <alimi.kmaruf@gmail.com>
2019-11-23 08:47:02 +0100

    ftadd-valiadation): validate comment details)

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import Articles from '../controllers/article';
 import ArticleInputValidation from '../middleware/article';
+import validateCommentInput from '../middleware/comment';
 import UserAuth from '../middleware/auth';
 
 const {
   createArticles, editAnArticle, deleteSelfArticle, createArticleComment, viewSpecificArticle
 } = Articles;
 const { validateArticlesDetails, validateASingleArticle } = ArticleInputValidation;
+const { validateCommentDetails } = validateCommentInput;
 const { verifyUserToken, isArticleOwner } = UserAuth;
 
 const articleRouter = express.Router();
@@ -14,7 +16,7 @@ const articleRouter = express.Router();
 articleRouter.post('/articles', verifyUserToken, validateArticlesDetails, createArticles);
 articleRouter.patch('/articles/:articleId', verifyUserToken, isArticleOwner, validateArticlesDetails, validateASingleArticle, editAnArticle);
 articleRouter.delete('/articles/:articleId', verifyUserToken, isArticleOwner, validateASingleArticle, deleteSelfArticle);
-articleRouter.post('/articles/:articleId/comment', verifyUserToken, validateASingleArticle, createArticleComment);
+articleRouter.post('/articles/:articleId/comment', verifyUserToken, validateCommentDetails, validateASingleArticle, createArticleComment);
 articleRouter.get('/articles/:articleId', verifyUserToken, viewSpecificArticle);
 
 
