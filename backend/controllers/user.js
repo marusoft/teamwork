f0a82e020af6883ed194298c0fcd07de0b3dc460
marusoft <alimi.kmaruf@gmail.com>
2021-03-23 23:55:03 +0100

    resolve

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -133,6 +133,7 @@ class Users {
   static async fetchLoggedInUser(req, res) {
     try {
       const value = [req.user.id];
+      console.log('ID', value);
       const { rows } = await pool.query(findUser, value);
       const {
         id, username, firstname, lastname, jobrole
@@ -166,6 +167,7 @@ class Users {
   static async fetchAUser(req, res) {
     try {
       const value = [req.params.id];
+      console.log('VALUE1', value);
       const { rows } = await pool.query(findUser, value);
       const {
         id, username, email, firstname, lastname, jobrole
